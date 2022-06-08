aab1b55b18314a9891f721e38234f10b1eba1329
Kazuya Takeshima <mail@mitukiii.jp>
2022-06-09 02:40:13 +0900

    Refactor

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ uak is update authorized_keys with specify url contents
 ## Usage
 
 ```
-$ uak <authorized_keys path> <remote authorized_keys url>
+$ uak <authorized keys path> <public keys url>
 ```
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{fs, io};
 use regex::Regex;
 use reqwest::StatusCode;
 
-static USAGE: &str = "Usage: uak <authorized_keys path> <remote authorized_keys url>";
+static USAGE: &str = "Usage: uak <authorized keys path> <public keys url>";
 
 static HEADER: &str = "\n# UAK BEGIN: The following lines is added by uak.\n\n";
 static FOOTER: &str = "\n\n# UAK END: The above lines is added by uak.\n";
@@ -14,21 +14,26 @@ static FOOTER: &str = "\n\n# UAK END: The above lines is added by uak.\n";
 type Result<T, E = Box<dyn Error + Send + Sync + 'static>> = core::result::Result<T, E>;
 
 fn main() {
-    let result = run();
-    result.unwrap_or_else(|err| {
+    let authorized_keys_path = env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("{}", USAGE);
+        std::process::exit(1);
+    });
+    let public_keys_url = env::args().nth(2).unwrap_or_else(|| {
+        eprintln!("{}", USAGE);
+        std::process::exit(1);
+    });
+
+    run(&authorized_keys_path, &public_keys_url).unwrap_or_else(|err| {
         eprintln!("uak: {}", err);
         std::process::exit(1);
     });
 }
 
-fn run() -> Result<()> {
-    let authorized_keys_path = env::args().nth(1).ok_or(USAGE)?;
-    let remote_authorized_keys_url = env::args().nth(2).ok_or(USAGE)?;
-    let current_content = read_file(&authorized_keys_path)?;
-    let content_to_add = read_url(&remote_authorized_keys_url)?;
+fn run(authorized_keys_path: &str, public_keys_url: &str) -> Result<()> {
+    let current_content = read_file(authorized_keys_path)?;
+    let content_to_add = read_url(public_keys_url)?;
     let edited_content = inject(&current_content, &content_to_add);
-    write_file(&authorized_keys_path, &edited_content)?;
-    Ok(())
+    write_file(authorized_keys_path, &edited_content)
 }
 
 fn read_file(path: &str) -> Result<String> {
@@ -46,15 +51,14 @@ fn write_file(path: &str, content: &str) -> Result<()> {
 fn read_url(url: &str) -> Result<String> {
     let mut content = String::new();
     let mut response = reqwest::blocking::get(url)?;
-    match response.status() {
-        StatusCode::OK => {
-            response.read_to_string(&mut content)?;
-            Ok(content)
-        }
-        _ => Err(Box::new(io::Error::new(
+    if response.status() == StatusCode::OK {
+        response.read_to_string(&mut content)?;
+        Ok(content)
+    } else {
+        Err(Box::new(io::Error::new(
             io::ErrorKind::Other,
             format!("{}", response.status()),
-        ))),
+        )))
     }
 }
 
